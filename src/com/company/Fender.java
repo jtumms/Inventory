983e4b48d5e985d872e3881389bd2139d5f12043
jtumms <john.tumminelli@tummsmedia.com>
2016-09-21 09:46:27 -0400

    First submit for 9/19/16 assignment

--- src/com/company/Fender.java
+++ src/com/company/Fender.java
@@ -4,19 +4,9 @@ package com.company;
  * Created by john.tumminelli on 9/20/16.
  */
 public class Fender extends Guitar {
-    String pickups;
-    String numPickups;
-
-    public Fender(String pickups, String numPickups) {
-        this.pickups = pickups;
-        this.numPickups = numPickups;
-        this.brand = "Fender";
-
-    }
-
-    public Fender(String itemNumber, String brand, int quantity, String pickups, String numPickups) {
-        super(itemNumber, brand, quantity);
-        this.pickups = pickups;
-        this.numPickups = numPickups;
+    public Fender(String itemNumber, int quantity) {
+        this.itemNumber = itemNumber;
+        this.quantity = quantity;
+        this.brand = "fender";
     }
 }
